4e1633ee01ffcd14f8c4f3229fe75be19665ebec
Belyashik2K <lovelybelyashik@gmail.com>
2025-02-24 00:09:53 +0300

    Remove commented-out leap year and date validation functions from date.h

--- src/database/types/date/date.h
+++ src/database/types/date/date.h
@@ -10,9 +10,6 @@ typedef struct Date {
     char *(*toString)(struct Date *self);
 } Date;
 
-
-// int is_leap_year(int year);
-// int is_valid_date(int day, int month, int year);
 Date *dateFactory(const char *dateString, const char *field);
 
 #endif
